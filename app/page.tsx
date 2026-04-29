26c6e2a45c8f9a3dcc106112436eeef13f5135e5
Lornah008 <lornahwanjiku008@gmail.com>
2026-04-29 06:06:12 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from 'next'
+import { Suspense } from 'react'
+import Loading from './loading'
+import FAQs from '@/components/FAQs'
+import Clients from '@/components/Clients'
+import Hero from '@/components/HeroSection'
+import TrackRecord from '@/components/TrackRecord'
+import WhyChooseUs from '@/components/WhyChooseUs'
+import CorePartnerShip from '@/components/CorePartnership'
+
+export const metadata: Metadata = {
+    title: 'Baldwin Enterprises',
+    description: 'Boost your signal today with our cell boosters.',
+}
+
+export default function Home() {
+    return (
+          <section >
+          
+                <Suspense fallback={<Loading />}>
+                
+                        <Hero />
+                        <WhyChooseUs />
+                        <Clients />
+                        <CorePartnerShip />
+                        <TrackRecord />
+                        <FAQs />
+                
+                </Suspense>Suspense>
+          </section>section>
+        )
+}
+</section>
